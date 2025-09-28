3b4b8cf05f76032596e33e862a847b35c81f9471
Jaspreet Singh Kalsi <bugsbunny@MacBook-Pro-2.local>
2025-09-28 15:35:51 -0400

    initial commit

--- src/Components/index.js
+++
@@ -1,5 +0,0 @@
-import Home from "./Home";
-import Header from "./Header";
-import Footer from "./Footer";
-
-export  { Header, Home, Footer };
--- src/consts.js
+++
@@ -1,6 +0,0 @@
-const consts = {
-  PHONE: "613-400-5735",
-  EMAIL: "info@punjabdriving.ca"
-};
-
-export default consts;
